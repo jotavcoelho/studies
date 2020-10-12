c0a1f6ab425a18936d51b4658ac9e8fc2998becb
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-10-12 18:42:39 -0300

    reactjs: chat now at least presentable, and started implementing states

--- reactjs/src/subjects/SocketIo/index.jsx
+++ reactjs/src/subjects/SocketIo/index.jsx
@@ -1,22 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import io from 'socket.io-client';
 
-import { Container, ChatBox, InputArea } from './styles';
+import { 
+  Container, 
+  ContactsContainer,
+  ChatContainer,
+  CurrentPerson,
+  ChatBox, 
+  InputArea,
+} from './styles';
 
 export default function SocketIo() {
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     const socket = io("http://localhost:3333");
-  });
+  }, []);
+
+  useEffect(() => {
+    
+  }, [message]);
+
+  function sendMessage() {
+    // send message
+  }
 
   return (
     <Container>
       <h1>Little real-time chat using Socket.io</h1>
-      <ChatBox></ChatBox>
-      <InputArea>
-        <input type="text"/>
-        <button type="button">Send</button>
-      </InputArea>
+      <ContactsContainer>
+        <h1>People</h1>
+      </ContactsContainer>
+      <ChatContainer>
+        <ChatBox>
+          <CurrentPerson>
+            <h2>Name</h2>
+          </CurrentPerson>
+        </ChatBox>
+        <InputArea>
+          <input 
+            type="text"
+            value={message}
+            onChange={e => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button 
+            type="button"
+            onClick={sendMessage}
+          >Send</button>
+        </InputArea>
+      </ChatContainer>
     </Container>
   )
 }
--- reactjs/src/subjects/SocketIo/styles.js
+++ reactjs/src/subjects/SocketIo/styles.js
@@ -7,20 +7,49 @@ export const Container = styled.div`
   background: #040108;
   border-radius: 12px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
+  position: relative;
 
   > h1 {
+    position: absolute;
     border: 1px solid #040108;
     border-radius: 12px;
-    margin-top: -15px;
+    top: -15px;
+    left: 25%;
     background: #040108;
-    padding: 0 6px;
+    padding: 0 24px;
   }
+`;
+
+export const ContactsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 0.3;
+`;
+
+export const ChatContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CurrentPerson = styled.div`
+  padding: 20px;
+  flex: 1;
+  background: #040108;
+  border-bottom: 1px solid #006e90;
+  border-radius: 12px;
 `;
 
 export const ChatBox = styled.div`
   flex: 1;
+  background: #07020d;
+  border-radius: 12px;
+  border-left: 1px solid #006e90;
+  border-right: 1px solid #006e90;
 `;
 
 export const InputArea = styled.div`
@@ -30,9 +59,9 @@ export const InputArea = styled.div`
   padding: 12px;
 
   input {
-    height: 30px;
+    height: 38px;
     width: 100%;
-    padding: 2px 4px;
+    padding: 2px 8px;
     margin-right: 12px;
     border: 1px solid #006e90;
     background: none;
