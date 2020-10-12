b1d0cf2c1fdb24b232ae3ced935d40b01fecafa0
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-10-12 14:45:51 -0300

    reactjs: now successfully connecting to websocket

--- reactjs/src/subjects/SocketIo/index.jsx
+++ reactjs/src/subjects/SocketIo/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import io from 'socket.io-client';
 
 import { Container } from './styles';
 
 export default function SocketIo() {
+  useEffect(() => {
+    const socket = io("http://localhost:3333");
+  });
+
   return (
     <h1>SocketIo</h1>
   )
