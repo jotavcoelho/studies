4aca0fdee1e570bab97e2a730fc4c4bb80a14735
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-20 07:34:54 -0300

    Vue: used  for altering stuff on higher order

--- vue/traversy-crash/src/components/Task.vue
+++ vue/traversy-crash/src/components/Task.vue
@@ -1,7 +1,8 @@
 <template>
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
-    <h3>{{ task.text }}
-      <i class="fas fa-times"></i>
+    <h3>
+      {{ task.text }}
+      <i @click="onDelete(task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -11,6 +12,11 @@ export default {
   name: 'Task',
   props: {
     task: Object
+  },
+  methods: {
+    onDelete(id) {
+      this.$emit('delete-task', id)
+    }
   }
 }
 </script>
