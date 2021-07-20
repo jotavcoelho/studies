d6fa5812f280ecc150b4cc0f8aa96d14b369c5b6
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-20 05:33:37 -0300

    Vue: used events for the first time

---
+++ vue/traversy-crash/src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button 
+    class="btn" 
+    :style="{ background: color }"
+    @click="onClick()"
+  >
+    {{text}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('clicked')
+    }
+  }
+}
+</script>
