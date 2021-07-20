cee4360ac3aef38961773739d10502fd853c3806
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-20 06:05:09 -0300

    Vue: first full single file componen

--- vue/traversy-crash/src/App.vue
+++ vue/traversy-crash/src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="container">
     <Header title="Hello asdas"/>
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'March 1s at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food shopping',
+        day: 'March 3rd at 11:00am',
+        reminder: false,
+      },
+    ]
   }
 }
 </script>
